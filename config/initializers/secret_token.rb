7edbc8cebbae6ce8ffdb3424d2bf9a2ba8935c67
KirilKostov <kiril_lol@abv.bg>
2013-07-11 18:16:30 +0300

    Part 2 - 9.D2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Depot::Application.config.secret_token = 'ba959dd47fa246fe5b8e0be173a9bb110e13f0c366c7f9c814357967647fa306ddd37837d396e145803aec4910be3290f0dfcd6ea10a7e68e6199b938f751fd6'
+Depot::Application.config.secret_key_base = 'xxxba959dd47fa246fe5b8e0be173a9bb110e13f0c366c7f9c814357967647fa306ddd37837d396e145803aec4910be3290f0dfcd6ea10a7e68e6199b938f751fd6'
